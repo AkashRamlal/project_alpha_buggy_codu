532fc68e5e61299730a397907fe7a2f06b3dd8ce
Vivesh Nidhansing <vivesh13@hotmail.com>
2026-09-09 11:35:45 +0200

    program with test message

--- project_alpha_buggy_coduV1/project_alpha_buggy_coduV1/Program.cs
+++ project_alpha_buggy_coduV1/project_alpha_buggy_coduV1/Program.cs
@@ -1,2 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Dit is een test.");
+﻿Console.WriteLine("Dit is een test.");
+Console.WriteLine("Test 2");
+
+int i = 0;
+
+while(i < 11)
+{
+    Console.WriteLine(i);
+    i++;
+}
