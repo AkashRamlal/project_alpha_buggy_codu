082ba6c66ea2e658a13e91e3b1ded1a2e8b0cf07
unknown <hindoe_memes@hotmail.com>
2026-09-17 17:06:20 +0200

    battle mechanic added

---
+++ project_alpha_buggy_coduV1/project_alpha_buggy_coduV1/Battle.cs
@@ -0,0 +1,114 @@
+public static class Battle
+{
+    public static void Start(Player player, Monster monster)
+    {
+        int monstersDefeated = 0;
+
+        while (monstersDefeated < 3 && player.CurrentHitPoints > 0)
+        {
+            // Reset monster health for the next fight
+            monster.CurrentHitPoints = monster.MaximumHitPoints;
+
+            Console.WriteLine();
+            Console.WriteLine(
+                $"A {monster.Name} appears! " +
+                $"({monstersDefeated + 1}/3)"
+            );
+
+            Console.WriteLine("Press ENTER to fight or E to escape.");
+
+            string input = Console.ReadLine()!.Trim().ToLower();
+
+            // Escape from battle
+            if (input == "e")
+            {
+                Console.WriteLine("You escaped from the battle!");
+                return;
+            }
+
+            // Only ENTER starts the fight
+            if (input != "")
+            {
+                Console.WriteLine("Invalid input.");
+                continue;
+            }
+
+            // Fight this monster
+            while (player.CurrentHitPoints > 0 &&
+                   monster.CurrentHitPoints > 0)
+            {
+                int playerDamage =
+                    World.RandomGenerator.Next(
+                        1,
+                        player.CurrentWeapon!.MaximumDamage + 1
+                    );
+
+                monster.CurrentHitPoints -= playerDamage;
+
+                Console.WriteLine(
+                    $"You hit the {monster.Name} " +
+                    $"for {playerDamage} damage."
+                );
+
+                // Monster defeated
+                if (monster.CurrentHitPoints <= 0)
+                {
+                    monstersDefeated++;
+
+                    Console.WriteLine(
+                        $"You defeated the {monster.Name}!"
+                    );
+
+                    Console.WriteLine(
+                        $"Monsters defeated: {monstersDefeated}/3"
+                    );
+
+                    break;
+                }
+
+                int monsterDamage =
+                    World.RandomGenerator.Next(
+                        1,
+                        monster.MaximumDamage + 1
+                    );
+
+                player.CurrentHitPoints -= monsterDamage;
+
+                Console.WriteLine(
+                    $"The {monster.Name} hits you " +
+                    $"for {monsterDamage} damage."
+                );
+
+                Console.WriteLine(
+                    $"Your health: " +
+                    $"{player.CurrentHitPoints}/" +
+                    $"{player.MaximumHitPoints}"
+                );
+
+                // Player died
+                if (player.CurrentHitPoints <= 0)
+                {
+                    Console.WriteLine("You died!");
+                    return;
+                }
+            }
+
+            // All 3 monsters defeated
+            if (monstersDefeated == 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine(
+                    $"You defeated all 3 {monster.Name}s!"
+                );
+
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(
+                "Press ENTER to fight the next monster " +
+                "or E to escape."
+            );
+        }
+    }
+}
--- project_alpha_buggy_coduV1/project_alpha_buggy_coduV1/Program.cs
+++ project_alpha_buggy_coduV1/project_alpha_buggy_coduV1/Program.cs
@@ -4,32 +4,47 @@
     {
         bool gameIsRunning = true;
         Location startingLocation = World.Locations[0];
-        Player player = new Player("AAA", 200, 200, startingLocation, null);
+
+        Player player = new Player(
+            "AAA",
+            200,
+            200,
+            startingLocation,
+            World.WeaponByID(World.WEAPON_ID_RUSTY_SWORD)
+        );
+
         while (gameIsRunning)
         {
             Tuple<String, Object> actionToPerform = player.CurrentLocation.Main(player);
+
             //USE actionToPerform.Item2 to get the appropriate parameter
             switch (actionToPerform.Item1)
             {
                 case "Travel":
                     player.CurrentLocation = (Location)actionToPerform.Item2;
                     break;
-                case "OpenInventory":
 
+                case "OpenInventory":
                     break;
+
                 case "EnterBattle":
                     Monster monsterToUse = (Monster)actionToPerform.Item2;
 
+                    Battle.Start(player, monsterToUse);
+
                     break;
+
                 case "TakeQuest":
                     if (player.CurrentQuest != null)
                     {
                         Console.WriteLine($"You are already on a quest: {player.CurrentQuest.Name}");
                         break;
                     }
+
                     if (player.CurrentLocation.QuestAvailableHere != null)
                     {
                         Quest quest = player.CurrentLocation.QuestAvailableHere;
+
                         Console.WriteLine($"Quest available: {quest.Name}");
                         Console.WriteLine(quest.Description);
                         Console.WriteLine("Do you want to accept the quest? (y/n)");
@@ -50,6 +65,7 @@
                     {
                         Console.WriteLine("There is no quest available here.");
                     }
+
                     break;
             }
         }
